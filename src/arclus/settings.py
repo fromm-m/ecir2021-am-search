ccb339b308ac2c8657438186f9234c4519b75236
sobermeier <obermesa@gmail.com>
2020-06-04 16:18:26 +0200

    fix flake in settings file

--- src/arclus/settings.py
+++ src/arclus/settings.py
@@ -2,7 +2,6 @@
 Global settings.
 """
 
-
 PREP_ASSIGNMENTS = "/nfs/data3/fromm/argument_clustering/data/preprocessed/assignments.csv"
 PREP_CLAIMS = "/nfs/data3/fromm/argument_clustering/data/preprocessed/claims.csv"
 PREP_PREMISES = "/nfs/data3/fromm/argument_clustering/data/preprocessed/premises.csv"
