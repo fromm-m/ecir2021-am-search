7c2efc857cf5e86866bb2f2fe9df9fec522f99a2
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-06-08 14:27:11 +0200

    Use real paths

--- src/arclus/settings.py
+++ src/arclus/settings.py
@@ -1,8 +1,9 @@
 """
 Global settings.
 """
+import pathlib
 
-
-PREP_ASSIGNMENTS = "/nfs/data3/fromm/argument_clustering/data/preprocessed/assignments.csv"
-PREP_CLAIMS = "/nfs/data3/fromm/argument_clustering/data/preprocessed/claims.csv"
-PREP_PREMISES = "/nfs/data3/fromm/argument_clustering/data/preprocessed/premises.csv"
+DATA_ROOT = pathlib.Path('/nfs/data3/fromm/argument_clustering/data')
+PREP_ASSIGNMENTS = DATA_ROOT / 'preprocessed' / 'assignments.csv'
+PREP_CLAIMS = DATA_ROOT / 'preprocessed' / 'claims.csv'
+PREP_PREMISES = DATA_ROOT / 'preprocessed' / 'premises.csv'
