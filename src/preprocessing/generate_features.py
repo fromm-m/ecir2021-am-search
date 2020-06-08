6b096cd913977a286e0b8b642a3e5de6c0a4f597
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-06-08 14:35:34 +0200

    Add script to pre-compute BERT embeddings

---
+++ src/preprocessing/generate_features.py
@@ -0,0 +1,36 @@
+"""
+Precompute BERT embeddings for claims and premises.
+"""
+import logging
+
+import numpy
+import pandas
+from bert_serving.client import BertClient
+
+from arclus.settings import PREP_CLAIMS, PREP_PREMISES
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    # Connect to BERT-as-a-service
+    client = BertClient()
+
+    for name, source_path, text_col_name in zip(
+        ('premises', 'claims'),
+        (PREP_CLAIMS, PREP_PREMISES),
+        ('premise_text', 'claim_text'),
+    ):
+        output_path = source_path.parent / source_path.name.replace('.csv', '.npy')
+        if output_path.is_file():
+            logging.info(f'Skipping existing file: {output_path}')
+            continue
+        df = pandas.read_csv(source_path, delimiter=',')
+        embeddings = client.encode(
+            texts=df[text_col_name].tolist(),
+        )
+        numpy.save(output_path, embeddings)
+
+
+if __name__ == '__main__':
+    main()
