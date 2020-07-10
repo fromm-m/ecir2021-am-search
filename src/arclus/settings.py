14193cc81683decb9bd0fdcdc160b58a8cb69cdc
Michael Fromm <michael.fromm@campus.lmu.de>
2020-07-10 14:37:57 +0200

    implemented the negative samples generation, negatives get saved as a csv file

--- src/arclus/settings.py
+++ src/arclus/settings.py
@@ -1,11 +1,20 @@
 """
 Global settings.
 """
+import pathlib
 # Preprocessing
-PREP_ASSIGNMENTS = "/nfs/data3/fromm/argument_clustering/data/preprocessed/assignments.csv"
-PREP_CLAIMS = "/nfs/data3/fromm/argument_clustering/data/preprocessed/claims.csv"
-PREP_PREMISES = "/nfs/data3/fromm/argument_clustering/data/preprocessed/premises.csv"
+DATA_ROOT = pathlib.Path('/nfs/data3/fromm/argument_clustering/data')
+PREP_ASSIGNMENTS = DATA_ROOT / 'preprocessed' / 'assignments.csv'
+PREP_CLAIMS = DATA_ROOT / 'preprocessed' / 'claims.csv'
+PREP_PREMISES = DATA_ROOT / 'preprocessed' / 'premises.csv'
+
+# Feature Generation
+CLAIMS_LENGTH = 19
+PREMISES_LENGTH = 220
+OUTPUT_FEATURES = DATA_ROOT / 'features' / 'embeddings.npy'
+
 
 # Negative Sampling
+NEGATIVE_SAMPLES = DATA_ROOT / 'preprocessed' / 'negatives.csv'
 # one negative per positive
 NUM_NEG_PER_POS = 1
