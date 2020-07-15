7fcd058b6a4a10475fb218e5bc16899fc6d3538d
sobermeier <obermesa@gmail.com>
2020-07-15 10:01:46 +0200

    add evaluation functions

---
+++ src/arclus/evaluation.py
@@ -0,0 +1,35 @@
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix, classification_report
+import torch
+
+
+def accuracy(
+    pred_y,
+    labels,
+    prob=True
+) -> torch.tensor:
+    if prob:
+        _, pred_y = torch.max(pred_y, dim=1)
+    return torch.tensor(torch.sum(pred_y == labels).item() / len(pred_y))
+
+
+def f1_macro(
+    pred_y,
+    labels,
+    prob=True
+) -> float:
+    if prob:
+        _, pred_y = torch.max(pred_y, dim=1)
+
+    f1_macro = f1_score(y_pred=pred_y, y_true=labels, average='macro')
+    return f1_macro
+
+
+def class_metrics(
+    pred_y,
+    labels,
+    prob=True
+):
+    if prob:
+        _, pred_y = torch.max(pred_y, dim=1)
+    return classification_report(labels, pred_y)
