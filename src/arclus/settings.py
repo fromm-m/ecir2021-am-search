7c4aee783e8643f5da61e738b232705fa31b0710
Michael Fromm <michael.fromm@campus.lmu.de>
2020-07-23 15:44:04 +0200

    bugfix

--- src/arclus/settings.py
+++ src/arclus/settings.py
@@ -2,16 +2,23 @@
 Global settings.
 """
 import pathlib
-# Preprocessing
+# Preprocessing Training-Data
 DATA_ROOT = pathlib.Path('/nfs/data3/fromm/argument_clustering/data')
 PREP_ASSIGNMENTS = DATA_ROOT / 'preprocessed' / 'assignments.csv'
 PREP_CLAIMS = DATA_ROOT / 'preprocessed' / 'claims.csv'
 PREP_PREMISES = DATA_ROOT / 'preprocessed' / 'premises.csv'
 
+# Preprocessing Test-Data
+PREP_ASSIGNMENTS_TEST = DATA_ROOT / 'preprocessed' / 'processedPremises.csv'
+PREP_CLAIMS_TEST = DATA_ROOT / 'preprocessed' / 'claim_test.csv'
+PREP_PREMISES_TEST = DATA_ROOT / 'preprocessed' / 'premises_test.csv'
+
 # Feature Generation
 CLAIMS_LENGTH = 19
 PREMISES_LENGTH = 220
 OUTPUT_FEATURES = DATA_ROOT / 'features' / 'embeddings.npy'
+CLAIMS_TEST_FEATURES = DATA_ROOT / 'features' / 'claims_test_embeddings.npy'
+PREMISES_TEST_FEATURES = DATA_ROOT / 'features' / 'premises_test_embeddings.npy'
 
 
 # Negative Sampling
