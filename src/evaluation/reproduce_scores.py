b3b5e4b36dbdb63edefed77c87ec6f9da37947a1
Michael Fromm <michael.fromm@campus.lmu.de>
2020-08-12 15:21:47 +0200

    refactoring

--- src/evaluation/reproduce_scores.py
+++ src/evaluation/reproduce_scores.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import math
 import argparse
+import itertools
+
 from arclus.settings import PREP_ASSIGNMENTS_TEST
 
 
@@ -81,6 +83,36 @@ def task_b(ordered_clusters: [pd.DataFrame]) -> [int]:
     return ranking
 
 
+def task_a(df_temp: pd.DataFrame, ordered_clusters: [pd.DataFrame]) -> [[int]]:
+    """
+        Calculates the ranking given the predicted_clusters, all combinations of premises are tried.
+        :param ordered_clusters: shape: (number_predicted_clusters)
+            The ordered_clusters containing the premises.
+        :return: ranking:
+            The ranking for all predicted clusters ordered by the cluster_id.
+        """
+    list = []
+    for cluster in ordered_clusters:
+        premise_ids = []
+        for index, row in cluster.iterrows():
+            premise_ids.append(row["resultClaimsPremiseID"])
+        list.append(premise_ids)
+
+    all_rankings = []
+    for combination in itertools.product(*list):
+        ranking = []
+        seen_gt_clusters = set()
+        for premise_id in combination:
+            premise_represent = df_temp.loc[df_temp["resultClaimsPremiseID"] == premise_id]
+            if premise_represent["premiseClusterID_groundTruth"].values[0] in seen_gt_clusters:
+                ranking.append(0)
+            else:
+                ranking.append(premise_represent["relevance"].values[0])
+                seen_gt_clusters.add(premise_represent["premiseClusterID_groundTruth"].values[0])
+        all_rankings.append(ranking)
+    return all_rankings
+
+
 def best_ranking(ordered_clusters: [pd.DataFrame]) -> [int]:
     """
     Calculates the best ranking given the groundtruth_clusters, as the representative the premise with the
@@ -106,11 +138,15 @@ def main():
     Calculates normalized discounted cumulative gain (nDCG) for all queries.
     """
     parser = argparse.ArgumentParser(description='Pre-compute BERT features.')
-    parser.add_argument('--algorithm', default='premiseClusterID_first512Tokens',
+    parser.add_argument('--algorithm', type=str, default='premiseClusterID_first512Tokens',
                         choices=['premiseClusterID_first512Tokens', 'premiseClusterID_slidingWindow',
                                  'premiseClusterID_sentences'], help='The algorithm which should be evaluated.')
+    parser.add_argument('--k', type=int, default=5, choices=[5, 10, ],
+                        help='The first k elements in the ranking should be considered')
+
     args = parser.parse_args()
     algorithm = args.algorithm
+    k = args.k
     df = pd.read_csv(PREP_ASSIGNMENTS_TEST, sep=";")
 
     # set the relevance to the according value (cf. paper)
@@ -119,6 +155,10 @@ def main():
     df['relevance'].loc[(df['relevance'] == "yesVeryRelevant")] = 2
 
     ndcg_list = []
+    ndcg_list_mean = []
+    ndcg_list_max = []
+    ndcg_list_min = []
+
     # Iterate over each claim
     for query in df["queryClaimID"].unique():
         # select all premises for a given claim
@@ -136,7 +176,7 @@ def main():
         # split premises according to gt clusters
         splitted_gt_clusters = split_clusters(df_temp, ordered_gt_cluster_ids, "premiseClusterID_groundTruth")
 
-        # calculate ranking of predicted_clusters
+        # calculate ranking of ordered_predicted_clusters
         predicted_ranking = task_b(splitted_prediction_clusters)
         print("predicted_ranking", predicted_ranking)
 
@@ -145,10 +185,22 @@ def main():
         print("gt_gain", gt_ranking)
 
         # calculate ndcg_score for given claim
-        ndcg_list.append(ndcg_score(y_score=predicted_ranking, y_true=gt_ranking, k=5))
+        ndcg_list.append(ndcg_score(y_score=predicted_ranking, y_true=gt_ranking, k=k))
 
-    # calculate the mean for all 30 query-claim
-    print(np.array(ndcg_list).mean())
+        # calculate ranking of all possible orders of clusters
+        # rankings = task_a(df_temp, splitted_prediction_clusters)
+        # scores = []
+        # for ranking in rankings:
+        #    scores.append(ndcg_score(y_score=ranking, y_true=gt_ranking, k=k))
+        # ndcg_list_mean.append(np.array([scores]).mean())
+        # ndcg_list_min.append(np.array([scores]).min())
+        # ndcg_list_max.append(np.array([scores]).max())
+
+    # calculate all evaluations scores
+    print("task _a mean:", "algorithm:", algorithm, "nDCG@", k, np.array(ndcg_list_mean).mean())
+    print("task _a min:", "algorithm:", algorithm, "nDCG@", k, np.array(ndcg_list_min).mean())
+    print("task _a max:", "algorithm:", algorithm, "nDCG@", k, np.array(ndcg_list_max).mean())
+    print("task _b ", "algorithm:", algorithm, "nDCG@", k, np.array(ndcg_list).mean())
 
 
 if __name__ == '__main__':
