b843a2fea3b6e8fa34e2c0cd5868d68b531822c7
fromm <Peterle2+->
2020-09-29 16:24:29 +0200

    3. baseline and small changes to other baselines (changed column name)

---
+++ executables/evaluation/baseline_3.py
@@ -0,0 +1,103 @@
+import argparse
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.cluster import KMeans
+
+from arclus.evaluation import best_ranking, ndcg_score, split_clusters
+from arclus.get_similar import LpSimilarity, get_most_similar
+from arclus.settings import CLAIMS_TEST_FEATURES, PREMISES_TEST_FEATURES, PREP_CLAIMS_TEST, PREP_PREMISES_TEST, \
+    PREP_ASSIGNMENTS_TEST
+from arclus.utils import load_assignments_with_numeric_relevance
+from transformers import BertModel, BertTokenizer, BertForSequenceClassification
+from torch.utils.data import (DataLoader, SequentialSampler)
+import logging
+from src.arclus.preprocessing.utils_am import load_and_cache_examples
+from tqdm import tqdm
+
+
+def load_bert_model_and_data(args):
+    tokenizer = BertTokenizer.from_pretrained(args.model_path)
+    bert_model = BertForSequenceClassification.from_pretrained(args.model_path)
+    data = load_and_cache_examples(args, args.task_name, tokenizer)
+    sampler = SequentialSampler(data)
+    return DataLoader(data, sampler=sampler, batch_size=args.batch_size), data, bert_model
+
+
+def inference(args, data, loader, logger, model):
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    predictions = []
+    logger.info("***** Running inference {} *****".format(""))
+    logger.info("  Num examples = %d", len(data))
+    logger.info("  Batch size = %d", args.batch_size)
+    model.to(device)
+    model.eval()
+    with torch.no_grad():
+        for batch in tqdm(loader, desc="Inference"):
+            batch = tuple(t.to(device) for t in batch)
+            with torch.no_grad():
+                inputs = {'input_ids': batch[0],
+                          'attention_mask': batch[1],
+                          'token_type_ids': batch[2], }
+                outputs = model(**inputs)
+                logits = outputs[0].cpu().numpy()[:, 1]
+                predictions.extend(logits.tolist())
+    return predictions
+
+
+def main():
+    logger = logging.getLogger(__name__)
+    parser = argparse.ArgumentParser(description='Pre-compute BERT features.')
+    parser.add_argument('--k', type=int, default=5, choices=[5, 10],
+                        help='The first k elements in the ranking should be considered')
+
+    parser.add_argument('--model_path', type=str, default="../../models/d3d4a9c7c23a4b85a20836a754e3aa56",
+                        help='Where the bert similarity model is located')
+    args = parser.parse_args()
+
+    args.data_dir = PREP_ASSIGNMENTS_TEST
+    args.task_name = "SIM"
+    args.overwrite_cache = True
+    args.max_seq_length = 512
+    args.model_type = "bert"
+    args.batch_size = 128
+    k = args.k
+
+    loader, data, model = load_bert_model_and_data(args)
+    predictions = inference(args, data, loader, logger, model)
+
+    df_assignments = load_assignments_with_numeric_relevance()
+    df_claims = pd.read_csv(PREP_CLAIMS_TEST)
+    df_premises = pd.read_csv(PREP_PREMISES_TEST)
+
+    ndcg_list = []
+    # iterate over all claims
+    start_id = 0
+    assert len(predictions) == len(df_assignments)
+    for index, row in df_claims.iterrows():
+        # claim id which we use right now
+        claim_id = row["claim_id"]
+
+        # locate all premises which are assigned to the current claim
+        premises = df_assignments.loc[df_assignments["claim_id"] == claim_id]
+        premises["similarity"] = predictions[start_id:start_id + len(premises)]
+
+        premises = premises.sort_values(by=['similarity'], ascending=False)
+
+        # generate the ranking (relevance) of the knn premises
+        predicted_ranking = premises.relevance.values
+
+        # groundtruth
+        ordered_gt_cluster_ids = premises["premiseClusterID_groundTruth"].sort_values().dropna().unique()
+        splitted_gt_clusters = split_clusters(premises, ordered_gt_cluster_ids, "premiseClusterID_groundTruth")
+        gt_ranking = best_ranking(splitted_gt_clusters)
+
+        # calculate nDCG for the given claim
+        ndcg_list.append(ndcg_score(y_score=predicted_ranking, y_true=gt_ranking, k=k))
+        start_id = start_id + len(premises)
+    print("task _b;", "algorithm:", "baseline_3", "nDCG@", k, np.array(ndcg_list).mean())
+
+
+if __name__ == '__main__':
+    main()
