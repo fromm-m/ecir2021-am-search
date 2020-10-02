34796dc60e03d25450a1c85c89d1b85a85d2bf96
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-10-02 17:31:26 +0200

    Fix style

--- executables/evaluation/evaluate_baselines.sh
+++ executables/evaluation/evaluate_baselines.sh
@@ -7,7 +7,7 @@ echo "[START] ${time}"
 method=zero_shot_knn
 for similarity in l2 l1 cos; do
   echo "$method $similarity"
-  python3 evaluate_baseline.py --method $method --similarity $similarity > /dev/null
+  python3 evaluate_baseline.py --method $method --similarity $similarity >/dev/null
 done
 
 # 2. Baseline
@@ -16,7 +16,7 @@ for cluster_ratio in 0.25 0.5 1.0; do
   for cluster_representative in closest-to-center closest-to-claim; do
     for similarity in l2 l1 cos; do
       echo "$method $cluster_ratio $similarity $cluster_representative"
-      python3 evaluate_baseline.py --method $method --similarity $similarity --cluster_ratio=$cluster_ratio --cluster_representative=$cluster_representative  > /dev/null
+      python3 evaluate_baseline.py --method $method --similarity $similarity --cluster_ratio=$cluster_ratio --cluster_representative=$cluster_representative >/dev/null
     done
   done
 done
