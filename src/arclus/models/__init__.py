b22b15c7ec0bea4a100b550dee271e34a5599137
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-10-05 18:37:12 +0200

    Add docstrings

--- src/arclus/models/__init__.py
+++ src/arclus/models/__init__.py
@@ -9,6 +9,7 @@ from ..utils import get_subclass_by_name
 
 
 def name_normalizer(name: str) -> str:
+    """Normalize the name of a method."""
     return name.lower().replace('_', '')
 
 
@@ -16,6 +17,17 @@ def get_baseline_method_by_name(
     name: str,
     **kwargs: Any,
 ) -> RankingMethod:
+    """
+    Instantiate a ranking method.
+
+    :param name:
+        The method name.
+    :param kwargs:
+        Additional keyword based arguments passed to the method's constructor.
+
+    :return:
+        An instatiated ranking method.
+    """
     cls = get_subclass_by_name(base_class=RankingMethod, name=name, normalizer=name_normalizer)
     real_kwargs = dict()
     for key, value in kwargs.items():
