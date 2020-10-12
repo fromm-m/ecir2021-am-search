241d4b5e4e90d9b8f7b7e0489815d5caf8ce57d5
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-10-12 14:06:22 +0200

    Add output

--- executables/preprocessing/generate_negative_samples.py
+++ executables/preprocessing/generate_negative_samples.py
@@ -8,26 +8,36 @@ from arclus.settings import NEGATIVE_SAMPLES, POSITIVE_SAMPLES, PREP_ASSIGNMENTS
 
 def main(*keywords: str):
     assignments = pd.read_csv(PREP_ASSIGNMENTS)
+    print('loaded', assignments.shape[0], 'positive samples.')
+
     claims = pd.read_csv(PREP_CLAIMS)
+    print('loaded', claims.shape[0], 'claims.')
 
     # filter claims by keywords
     claims: pd.DataFrame = claims.loc[claims["claim_text"].str.contains('|'.join(f'({key})' for key in keywords), regex=True), ["claim_id"]]
+    print('Kept', claims.shape[0], 'claims.')
+
     pairs = claims.merge(right=assignments, how="inner", on="claim_id")
 
     # save positive pairs
     pairs.to_csv(POSITIVE_SAMPLES)
+    print('wrote', pairs.shape[0], 'positive samples.')
 
     # save all negative pairs
-    all_positives = set(zip(pairs["premise_id"].tolist(), pairs["claim_id"].tolist()))
-    all_premises, all_claims = set(pairs[col].unique().tolist() for col in ["premise_id", "claim_id"])
+    all_premises = set(pairs["premise_id"].unique().tolist())
+    pool = dict()
+    for claim_id, group in pairs.groupby(by='claim_id'):
+        pool[claim_id] = all_premises.difference(group["premise_id"].unique().tolist())
+    print('In total', sum(map(len, pool.values())), 'negative samples.')
+
     all_negatives = {
         (pid, cid)
-        for pid in all_premises
-        for cid in all_claims
-        if (pid, cid) not in all_positives
+        for pid, cids in pool.items()
+        for cid in cids
     }
     negative_pairs = pd.DataFrame(data=list(all_negatives), columns=["premise_id", "claim_id"])
     negative_pairs.to_csv(NEGATIVE_SAMPLES)
+    print('wrote', negative_pairs.shape[0], 'negative samples.')
 
 
 if __name__ == '__main__':
@@ -35,4 +45,6 @@ if __name__ == '__main__':
     parser.add_argument('--keyword', nargs='+', type=str)
     args = parser.parse_args()
 
-    main(keywords=args.keyword)
+    keywords = 'energy, gas, fuel, solar, global warming, natural source, nuclear, renewable, climate change, opec, facturing, fusion, pollution, biofuels, wind'.split(sep=', ')
+
+    main(*keywords)
